0aa91f8a56201fdc6874a444b21a88235b4dc328
Ayush <ayush@coursera.com>
2020-06-18 23:20:06 +0530

    My first AngularJS App

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 (function(){
+ 'use strict';
 
 angular.module('myFirstApp', [])
 
-.controller('Myfirstcontroller', function () {
+.controller('Myfirstcontroller', function ($scope) {
+  $scope.name="Ayush";
 
 });
 
